07c006e19d427daf0ddbf0a04b69083bfacfc14e
Diya Maji <150379399+DiyaMaji22@users.noreply.github.com>
2026-02-17 13:55:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int size=nums.size();
+        int start=0;
+        int end=size-1;
+        while(start<end){
+            int mid=start+(end-start)/2
+
+            if(arr[mid])
+        }
+        
+    }
+};
