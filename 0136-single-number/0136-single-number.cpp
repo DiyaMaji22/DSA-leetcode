f3e2286c86726f4fedf59661000a3926b97e6389
Diya Maji <150379399+DiyaMaji22@users.noreply.github.com>
2026-02-19 16:46:00 +0530

    Time: 10 ms (14.8%), Space: 23.9 MB (8%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int length=nums.size();
+        unordered_map<int,int>freq;
+        for(int num:nums){
+            freq[num]++;
+        }
+        for(auto&pair:freq){
+            if(pair.second==1){
+                return pair.first;
+            }
+        }
+     return 1;   
+    }
+};
