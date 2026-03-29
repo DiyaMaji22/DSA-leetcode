313e9731d1171264b10e6537f4549efe3d8d5388
Diya Maji <150379399+DiyaMaji22@users.noreply.github.com>
2026-03-30 01:45:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (84.82%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int res=0 ,curr=0;
+        for(char c:s){
+            if(c=='('){
+                curr++;
+                res=max(res,curr);
+            }
+            else if(c==')'){
+                curr--;
+            }
+        }
+        return res;
+    }
+};
