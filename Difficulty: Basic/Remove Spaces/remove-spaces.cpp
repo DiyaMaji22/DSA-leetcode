b7f93c3d57d0fb27df105ecb96224acf311fb90d
Diya Maji <150379399+DiyaMaji22@users.noreply.github.com>
2026-06-23 14:21:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+        string ans="";
+        for(char c:s){
+            if(c==' '){
+                continue;
+            }
+            else{
+                ans.push_back(c);
+            }
+            
+        }
+        return ans;
+        
+    }
+};
