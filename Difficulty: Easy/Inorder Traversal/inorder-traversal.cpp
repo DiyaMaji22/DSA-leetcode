8b15d261aebdaee04cda194f005cb15033835559
Diya Maji <150379399+DiyaMaji22@users.noreply.github.com>
2026-08-01 10:55:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.cpp
@@ -0,0 +1,30 @@
+/* Structure of Binary Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    void inorder(Node*root,vector<int>&res){
+        if(root==nullptr) return;
+        inorder(root->left, res);
+        res.push_back(root->data);
+        inorder(root->right, res);
+    }
+    vector<int> inOrder(Node* root) {
+        // code here
+        vector<int>res;
+        inorder(root,res);
+        return res;
+        
+        
+    }
+};
