d39259877fddbd59ce98a3ddb58813b33cc4c8f4
Diya Maji <150379399+DiyaMaji22@users.noreply.github.com>
2026-08-21 19:49:50 +0530

    Time: 3 ms (33.79%), Space: 45.8 MB (51.54%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void solve (int[] candidates,List<List<Integer>>ans,List<Integer>output,int sum,int index,int target){
+        if(index >= candidates.length){
+            if(target==sum){
+                ans.add(new ArrayList<>(output));
+            }
+            return;
+        }
+        if (index >= candidates.length || sum > target) {
+            return;
+        }
+
+        output.add(candidates[index]);
+        sum=sum+candidates[index];
+        solve(candidates,ans,output,sum,index,target);
+
+        output.remove(output.size()-1);
+        sum=sum-candidates[index];
+
+       solve(candidates,ans,output,sum,index+1,target);
+        
+
+        
+
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>output=new ArrayList<>();
+        solve(candidates,ans,output,0,0,target);
+        return ans;
+    }
+}
